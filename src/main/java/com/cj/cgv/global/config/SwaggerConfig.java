bf7ceab6b7bdd815e19f9d37a9be5254c76233d5
kangwook1 <rkddnr2078@naver.com>
2025-05-09 10:25:32 +0900

    [Feat] 스웨거 https 요청 추가

--- src/main/java/com/cj/cgv/global/config/SwaggerConfig.java
+++ src/main/java/com/cj/cgv/global/config/SwaggerConfig.java
@@ -1,14 +1,33 @@
 package com.cj.cgv.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${deployment.server.url}")
+    private String serverUrl;
+
     private Info apiInfo() {
         return new Info()
                 .title("CGV API")
                 .description("CGV API 설명서")
                 .version("1.0.0");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .servers(List.of(
+                        new Server().url("http://localhost:8080/api"),
+                        new Server().url(serverUrl)));
     }
 }
