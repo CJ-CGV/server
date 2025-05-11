d9b5d417324493015dcfee55424c0d09a10a8864
kangwook1 <rkddnr2078@naver.com>
2025-05-11 20:04:54 +0900

    Route53 도메인 Cors 설정

--- src/main/java/com/cj/cgv/global/config/CorsConfig.java
+++ src/main/java/com/cj/cgv/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://d3rsok50zivkth.cloudfront.net")
+                .allowedOriginPatterns("https://dudghkrhks.store")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
